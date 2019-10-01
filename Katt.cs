c50604aeec04bbd3705a1e690171fbbe83e24028
Myran <myrankalander@gmail.com>
2019-10-01 11:40:06 +0200

    part 3 done

--- Katt.cs
+++ Katt.cs
@@ -8,6 +8,13 @@ namespace excercises_classes
         private int ålder;
         private string ras;
         
+        public Katt(string namn, int ålder, string ras)
+        {
+            this.namn = namn;
+            this.ålder = ålder;
+            this.ras = ras;
+        }
+
         public string Namn
         {
             get;
@@ -23,7 +30,10 @@ namespace excercises_classes
             get;
             set;
         }
-
+        public void KattInfo()
+        {
+            Console.WriteLine("Min katt heter "+namn+", hon är "+ålder+" år gammal och hon är en "+ras+".");
+        }
         public void Bark()
         {
             Console.WriteLine("Katter skäller inte, knäppis!");
--- Program.cs
+++ Program.cs
@@ -6,12 +6,9 @@ namespace excercises_classes
     {
         static void Main(string[] args)
         {
-            Katt minKatt = new Katt();
-            minKatt.Namn = "Zelda";
-            minKatt.Ålder = 1;
-            minKatt.Ras = "sibirisk katt";
-
-            Console.WriteLine("Min katt heter "+minKatt.Namn+", hon är "+minKatt.Ålder+" år gammal och hon är en "+minKatt.Ras+".");
+            Katt minKatt = new Katt("Zelda", 1, "sibirisk katt");
+            
+            minKatt.KattInfo();
             minKatt.Bark();
             minKatt.Fetch();
         }
